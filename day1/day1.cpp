55ec6ccb9d5c721dd8c4dfdb6e06cc4da0fc0e99
kingdiesel <aspivak@gmail.com>
2025-12-01 20:48:58 -0600

    day 1 part 2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -42,9 +42,52 @@ public:
         }
     }
 
+    int ConsumeInstruction2(const Instruction& instruction)
+    {
+        int dial_start = value;
+        int num_zeroes = instruction.amount / 100;
+        //if (num_zeroes > 0)
+        //{
+        //    std::cout << "hello";
+        //}
+        int modified_amount = instruction.amount % 100;
+        if (instruction.dir == Direction::LEFT)
+        {
+            modified_amount *= -1;
+        }
+
+        value += modified_amount;
+        if (value < 0)
+        {
+            value += 100;
+            if (dial_start != 0)
+            {
+                num_zeroes++;
+            }
+        }
+        else if (value >= 100)
+        {
+            value -= 100;
+            if (dial_start != 0)
+            {
+                num_zeroes++;
+            }
+        }
+        else if (dial_start != 0)
+        {
+            num_zeroes += value == 0 ? 1 : 0;
+        }
+        return num_zeroes;
+    }
+
     int GetValue() const
     {
         return value;
+    }
+
+    void Reset()
+    {
+        value = 50;
     }
 private:
     int value = 50;
@@ -84,6 +127,15 @@ int main()
     {
         dial.ConsumeInstruction(instruction);
         num_zeroes += dial.GetValue() == 0 ? 1 : 0;
+    }
+
+    std::cout << num_zeroes << std::endl;
+
+    dial.Reset();
+    num_zeroes = 0;
+    for (const Instruction instruction : instructions)
+    {
+        num_zeroes += dial.ConsumeInstruction2(instruction);
     }
 
     std::cout << num_zeroes << std::endl;
